c52f8e0fb9f15f36d604a4b6e4da62b8d378bd9b
Jasper Soetendal <jasper@soetendal.nl>
2022-01-19 11:23:50 +0100

    Create redirect.php

---
+++ redirect.php
@@ -0,0 +1,45 @@
+<?php
+    error_reporting(E_ALL ^E_NOTICE ^E_WARNING);
+    $url = $_SERVER['REQUEST_URI'];
+    //$url = '/node//505';
+    if($_SERVER["SERVER_NAME"] == "localhost") {
+        $parts = explode("/", strtolower($url));
+        array_splice($parts, 0,1);
+    } else {
+        $parts = explode("/", strtolower($url));
+    }
+    $settings = json_decode(file_get_contents("app/settings/settings.json"));
+    $module = null;
+    $node = null;
+    $nodes = null;
+
+    $modules = [];
+    foreach($settings->modules as $m){
+        $modules[] = $m->name;
+    }
+
+
+    if(strtolower($parts[1] == "nodes")){
+        $type = $parts[2];
+        foreach($settings->content->entities as $e) {
+            if (strtolower(trim($e->type)) == strtolower(trim($type))) {
+                print("Location: https://www.datapedia.nl/". $e->module . $url); exit();
+                header("Location: https://www.datapedia.nl/". $e->module . $url, true, 301); exit();
+            }
+        }
+    }elseif(strtolower($parts[1] == "node")){
+        $nodeId = $parts[3];
+        $node = json_decode(file_get_contents("https://www.datapedia.nl/api/public/node/get/" . $nodeId));
+        $type = $node->type;
+        foreach($settings->content->entities as $e) {
+            if (strtolower(trim($e->type)) == strtolower(trim($type))) {
+                print("Location: https://www.datapedia.nl". $e->module . $url); exit();
+                header("Location: https://www.datapedia.nl/". $e->module . $url, true, 301); exit();
+            }
+        }
+    } else if(in_array($parts[1], $modules)){
+        header("Location: https://www.datapedia.nl/". $url, true); exit();
+    } else {
+        header("Location: https://www.datapedia.nl/"); exit();
+    }
+?>
